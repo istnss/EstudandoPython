b3a5488df5d4cbe96c7feb2604f5164921c25d0b
Isabela <115189675+Istnss@users.noreply.github.com>
2023-06-03 18:56:43 -0300

    Add files via upload
    Código com limitações, parece estar em loop infinito, tentei try/except, mas não funcionou. Também tentei if/else e por último funções, mas sem sucesso também.

---
+++ exercicioTerreno.py
@@ -0,0 +1,17 @@
+"""Problema "terreno"
+Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular 
+com uma casa decimal, bem como o valor do metro quadrado do terreno com duas casas 
+decimais. Em seguida, o programa deve mostrar o valor da área do terreno, bem como o 
+valor do preço do terreno, ambos com duas casas decimais, conforme exemplo."""
+
+largura = float(input("Digite a largura do terreno: "))
+comprimento = float(input("Digite o comprimento do terreno: "))
+metroQuadrado = float(input("Digite o valor do metro quadrado do terreno: "))
+
+
+area = largura * comprimento
+preco = area * metroQuadrado
+print("Area do terreno = ", area, '\n' "Preço do terreno = ", preco)
+
+
+
