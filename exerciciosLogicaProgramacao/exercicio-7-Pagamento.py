3f815580ac2da4854aa42d7cf1df8bd0815a2670
Isabela <115189675+istnss@users.noreply.github.com>
2024-04-18 19:08:48 -0300

    Update and rename exercicioPagamento.py to exercicio-7-Pagamento.py

--- exerciciosLogicaProgramacao/exercicio-7-Pagamento.py
+++ exerciciosLogicaProgramacao/exercicio-7-Pagamento.py
@@ -1,6 +1,5 @@
 """Problema "pagamento"
-Fazer um programa para ler o nome de um(a) funcionário(a), o valor que ele(a) 
-recebe por hora, e a quantidade de horas trabalhadas por ele(a). Ao final, 
+Fazer um programa para ler o nome de um(a) funcionário(a), o valor que ele(a) recebe por hora, e a quantidade de horas trabalhadas por ele(a). Ao final, 
 mostrar o valor do pagamento do funcionário com uma mensagem explicativa."""
 
 
@@ -9,9 +8,9 @@ print(inf.upper())
 #the upper() method converts all lowercase characters in a string into uppercase.
 
 nome = input("Digite o nome do funcionário: ")
-valorHora = int(input("Digite o valor por hora trabalhada: "))
+valorHora = float(input("Digite o valor por hora trabalhada: "))
 horaTrabalhada = int(input("Digite a quantidade de horas trabalhadas: "))
 
 pagamento = valorHora * horaTrabalhada
 
-print("O pagamento para ",nome, "deve ser igual a ",pagamento)
+print(f'O pagamento para {nome} deve ser de {pagamento:.2f}')
