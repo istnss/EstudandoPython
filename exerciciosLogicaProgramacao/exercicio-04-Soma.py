2681ec214f7c5e355864b070c83fd263290a4b3d
Isabela <115189675+istnss@users.noreply.github.com>
2024-04-18 19:59:14 -0300

    Delete exerciciosLogicaProgramacao directory

--- exerciciosLogicaProgramacao/exercicio-04-Soma.py
+++
@@ -1,8 +0,0 @@
-"""Problema "soma"
-Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela  o valor da soma destes números."""
-
-x = int(input("Digite um número: "))
-y = int(input("Digite outro número: "))
-
-soma = x + y
-print('Soma =', soma)
