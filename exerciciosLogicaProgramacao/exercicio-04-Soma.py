72acbcb3c5dfc86ec99c4daf45645d50474d867a
Isabela <115189675+istnss@users.noreply.github.com>
2024-04-18 19:12:46 -0300

    Update and rename exercicioSoma.py to exercicio-04-Soma.py

---
+++ exerciciosLogicaProgramacao/exercicio-04-Soma.py
@@ -0,0 +1,8 @@
+"""Problema "soma"
+Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela  o valor da soma destes números."""
+
+x = int(input("Digite um número: "))
+y = int(input("Digite outro número: "))
+
+soma = x + y
+print('Soma =', soma)
--- exerciciosLogicaProgramacao/exercicioSoma.py
+++
@@ -1,9 +0,0 @@
-"""Problema "soma"
-Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela 
-o valor da soma destes números."""
-
-x = float(input("Digite um número: "))
-y = float(input("Digite outro número: "))
-
-soma = x + y
-print("A soma entre ", x, "e ", y, "é igual a ", soma)
